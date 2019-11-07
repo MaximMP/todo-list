c1480e8711b2ab0c02bb099556150fd766c65539
MaximMP <zo.bordeini@gmail.com>
2019-11-07 19:17:49 +0200

    new file

---
+++ Script.js
@@ -0,0 +1,15 @@
+const tasks = [
+  {
+    id: 1,
+    title: "Выучить, 35",
+    Text: "Ходить, на, курсы, делать д/з",
+    completed: false
+  },
+  {
+    id: 1,
+    title: "Выучить React",
+    Text: "Ходить на курсы,делать д/з",
+    completed: false
+  }
+];
+console.log("hello");
